1cd792d5950dff2b4f5613cc539bacd3bc287c32
Robert Medina <robertsmedina11@gmail.com>
2024-04-05 17:26:13 -0400

    how to apply context guided project

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,15 @@ import './styles/styles.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+import { Quotesprovider } from './context/quotesContext'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
+  <Quotesprovider>
   <App />
+
+  </Quotesprovider>
+
 )
